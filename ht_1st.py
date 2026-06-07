4191ae490825573194b4196c282100519e624db2
enya-1234 <silentlygoingto@gmail.com>
2026-06-07 21:41:58 +0900

    Update and rename ht_main.py to ht_1st.py

--- ht_1st.py
+++ ht_1st.py
@@ -4,18 +4,17 @@ from pathlib import Path
 import streamlit as st
 #file_path = Path("desktop", "cpp", "ht") / "ht.png"
 file_path = Path("ht.png")
-st.write('view')
-st.write("view1")
-if file_path.exists():
-    st.write('exist')
+st.title('view')
+#if file_path.exists():
+#    st.write('exist')
 
-else:
-    st.write('errorrr')
+#else:
+#    st.write('errorrr')
 
 view = [1, 2, 3, 4, 5]
 view
+
 file_content = file_path.read_bytes()
-st.write('view2')
 #with open(file_path, "rb") as file:
 #    file_content = file.read()
 
@@ -25,4 +24,4 @@ st.download_button(
     file_name="ht.png",
     mime="image/png",
     )
-st.write('view4')
+st.link_button("For more  information:", "https://www.science.org/")
